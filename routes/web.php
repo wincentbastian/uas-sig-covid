e934ee56f22b7c1c11d6bc9338265bd517d02121
DarkStarKhazix <wincentbastian3@gmail.com>
2020-06-19 21:19:22 +0800

    last commit

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,7 @@ Route::resource('/admin/pasien','PasienController');
 
 Route::prefix('admin')->group(function () {
   
-    Route::get('/pasien', 'PasienController@index')->name('pasien');
-    Route::post('/pasien', 'PasienController@store')->name('pasien-store');
-    Route::get('/pasien/{tanggal}', 'PasienController@show')->name('pasien-show');
-    Route::put('/pasien/{id}', 'PasienController@update')->name('pasien-update');
-    Route::delete('/pasien/{id}', 'PasienController@destroy')->name('pasien-delete');
 
-    Route::get('/kabupaten', 'KabupatenController@index')->name('kabupaten');
 
     Route::get('/pasien-per-kelurahan', 'PasienPerKelurahanController@index')->name('pasien-per-kelurahan');
     Route::post('/pasien-per-kelurahan', 'PasienPerKelurahanController@store')->name('pasien-per-kelurahan-store');
